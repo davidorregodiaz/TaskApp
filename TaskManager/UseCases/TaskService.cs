3c3058f90c768ec3c9e2e9a73f3d40beed24e7d7
David Orrego <orregod091@gmail.com>
2025-03-26 15:47:49 -0400

    Create Class TaskValidation.cs

---
+++ TaskManager/UseCases/TaskService.cs
@@ -0,0 +1,74 @@
+using System.Text.Json;
+using TaskManager.Domain;
+using TaskManager.Domain.Errors;
+using TaskManager.Interfaces;
+using TaskManager.UseCases.Validation;
+
+namespace TaskManager.UseCases;
+
+public class TaskService : ITaskService
+{
+    public List<TodoTask>? Tasks;
+    private readonly ITaskReader _reader;
+    private readonly TaskValidation _taskValidation;
+    
+    public TaskService(ITaskReader reader, TaskValidation taskValidation)
+    {
+        _reader = reader;
+        _taskValidation = taskValidation;
+        Tasks = LoadTasksStored();
+    }
+
+
+    public void CreateTask(string createCommand)
+    {
+        if (!string.IsNullOrEmpty(createCommand))
+        {
+            var cmds = createCommand.Split(" ");
+            try
+            {
+                if (_taskValidation.ValidateTask(cmds))
+                {
+                    string taskDescription = string.Join(" ",cmds.Skip(2));
+                    TodoTask newTask = new TodoTask { Description = taskDescription };
+                    Tasks?.Add(newTask);
+                    StoreTasks(Tasks!);
+                    Console.WriteLine($"Created new task: {taskDescription} , Id: {newTask.Id}");
+                }
+            }
+            catch (AppException appException)
+            {
+                Console.WriteLine(appException.Message);
+            }
+        }
+    }
+
+    public void UpdateTask(string taskId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteTask(string taskId)
+    {
+        throw new NotImplementedException();
+    }
+    
+
+    public void StoreTasks(List<TodoTask> tasksToStore)
+    {
+        string path = "C:\\Users\\david\\RiderProjects\\TaskManager\\TaskManager\\Data";
+        var jsonOptions = new JsonSerializerOptions{WriteIndented = true};
+        var jsonString = JsonSerializer.Serialize(tasksToStore, jsonOptions);
+        File.WriteAllText(Path.Combine(path,"tasks.json"),jsonString);
+    }
+
+    public void UpdateStatus(string taskId,string status)
+    {
+        throw new NotImplementedException();
+    }
+
+    private List<TodoTask>? LoadTasksStored()
+    {
+        return _reader.GetTasks();
+    }
+}

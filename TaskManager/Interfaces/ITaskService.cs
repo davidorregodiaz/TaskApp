35f7745094f5084aa7e890e8098c41ea03a620d6
David Orrego <orregod091@gmail.com>
2025-03-27 18:15:15 -0400

    add methods CreateTaskTable,CreateInProgressTaskTable,CreateDoneTaskTable

--- TaskManager/Interfaces/ITaskService.cs
+++ TaskManager/Interfaces/ITaskService.cs
@@ -5,8 +5,10 @@ namespace TaskManager.Interfaces;
 public interface ITaskService
 {
     void CreateTask(string taskDescription);
-    void UpdateTask(string taskId);
-    void DeleteTask(string taskId);
-    void StoreTasks(List<TodoTask> tasksToStore);
-    void UpdateStatus(string taskId,string status);
+    void UpdateTask(Guid taskId,string taskDescription);
+    void DeleteTask(Guid taskId);
+    void UpdateStatus(Guid taskId,string status);
+    void CreateTaskTable();
+    void CreateDoneTaskTable();
+    void CreateInProgressTaskTable();
 }
